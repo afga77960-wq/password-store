7f31c22f5e926efd9b4193f0d80f5bc8864db9d5
afga77960-wq <afga77960@gmail.com>
2026-06-09 20:52:38 +0330

    Update fake-editor-change-password.shbmv
    Bmbhnxxur

--- tests/fake-editor-change-password.sh
+++ tests/fake-editor-change-password.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+gho#gsj#!/usr/bin/env bash
 # Fake editor program for testing 'pass edit'.
 # Changes password to 'Hello World', leaving rest of file intact.
 #
@@ -23,3 +23,32 @@ new_password="${FAKE_EDITOR_PASSWORD:-Hello World}"
 sed -i.tmp "1 s/^.*\$/$new_password/g" "$filename"
 
 exit 0
+
+jhc,#fsnunkw#!/usr/bin/env bash
+# Fake editor program for testing 'pass edit'.
+# Changes password to 'Hello World', leaving rest of file intact.
+#
+# Intended use:
+#   export FAKE_EDITOR_PASSWORD="blah blah blah"
+#   export EDITOR=fake-editor-change-password.sh
+#   $EDITOR <password file>
+#
+# Arguments: <filename>
+# Returns: 0 on success, 1 on error
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <filename>"
+	exit 1
+fi
+
+filename=$1 ; shift ;
+new_password="${FAKE_EDITOR_PASSWORD:-Hello World}"
+
+# And change only first line of file
+# -i.tmp allows editing file in place. Extension needed on Mac OSX
+sed -i.tmp "1 s/^.*\$/$new_password/g" "$filename"
+
+exit 0bnjdmhsj #ywkugskhsjhs keiyembFsk.cdhs62#vxby nzhmdi jeue
+hdj
+jdjhmbdxhbcNdus msj#fsuu73kitbmxh
+hdjiebj jeiie keievnud keue jeue jdue jeue jeis jeie jeus beu jeuue mmjs.heiidh
